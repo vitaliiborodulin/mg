1f08d8c1814f85dc0f72535e8e46ab1dcbfacc51
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2020-07-12 11:23:03 +0300

    fix faq

--- src/js/components/faq.js
+++ src/js/components/faq.js
@@ -1,11 +1,14 @@
-$('.faq__answer:first').show()
-$('.faq__question:first').addClass('faq__question--open');
+// $('.faq__answer:first').show()
+// $('.faq__question:first').addClass('faq__question--open');
 
 $('.faq__question').on('click', function(){
+    var question = $(this);
     var answer = $(this).next();
-    $('.faq__question').removeClass('faq__question--open');
-    $(this).addClass('faq__question--open');
 
-    $('.faq__answer:visible').not(answer).hide(200);
-    answer.slideDown(200);
+    $('.faq__question').not(question).removeClass('faq__question--open');
+    question.toggleClass('faq__question--open');
+
+    $('.faq__answer:visible').not(answer).slideUp(200);
+    answer.slideToggle(200);
+
 })
