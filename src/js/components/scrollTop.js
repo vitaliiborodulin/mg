4e0c93c50b6722e10ea6a6b0a66623cf27f31fe7
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2020-07-12 11:44:35 +0300

    fix time

--- src/js/components/scrollTop.js
+++ src/js/components/scrollTop.js
@@ -15,6 +15,6 @@ $('body').on('click', '[href*="/#"]', function(e){
     var fixed_offset = 85;
     $('html,body').stop().animate({ 
       scrollTop: $(this.hash).offset().top - fixed_offset 
-    }, 1000);
+    }, 300);
   e.preventDefault();
 });
